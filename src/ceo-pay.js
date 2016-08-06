5f2783bb3ede3f113b9fac5001b62accf624abf1
Andy Shora <andyshora@gmail.com>
2016-08-06 12:03:48 +0100

    1 worker v 1 ceo

--- src/ceo-pay.js
+++ src/ceo-pay.js
@@ -7,32 +7,37 @@ export class Viz {
     this._system = system; // ref to the florajs system
     this._world = world;
     this._workers = [];
+    this._dollars = [];
 
-    this._createWorkers(50);
+    this._ceoDollars = [];
+    this._ceo = null;
+
+    this._createWorkers(1);
+    this._createCEO(500);
   }
 
   _createWorkers(numWorkers) {
 
     for (let i = 0; i < numWorkers; i++) {
 
-      const worker = this._system.add('Walker', {
-        color: [215,239,242],
+      const dollar = this._system.add('Walker', {
+        color: [97,204,162],
         opacity: 1,
-        width: 10,
-        height: 10
+        width: 3,
+        height: 3
       });
 
-      this._workers.push(worker);
+      this._dollars.push(dollar);
 
-      // add an agent (a wealth coin) which seeks the worker
-      this._system.add('Agent', {
-        seekTarget: worker,
-        color: [204,162,97],
+      const worker = this._system.add('Agent', {
+        seekTarget: dollar,
+        color: [255,255,255],
+        borderRadius: 100,
         motorSpeed: 2,
         minSpeed: 1,
-        maxSpeed: 10,
-        width: 5,
-        height: 5,
+        maxSpeed: 5,
+        width: 10,
+        height: 10,
         borderWidth: 0,
         location: new Flora.Vector(this._world.width * 0.49, 0)
         /*sensors: [
@@ -46,8 +51,42 @@ export class Viz {
         ]*/
       });
 
+      this._workers.push(worker);
+
+    }
+
+      // add an agent (a wealth coin) which seeks the worker
+
+  }
+  _createCEO(numDollars) {
+
+    log.debug('_createCEO', numDollars);
+
+    this._ceo = this._system.add('Walker', {
+      color: [255,255,0],
+      opacity: 1,
+      width: 10,
+      height: 10,
+      maxSpeed: 0.5
+    });
+
+    for (let i = 0; i < numDollars; i++) {
+      const ceoDollar = this._system.add('Agent', {
+        seekTarget: this._ceo,
+        borderRadius: 100,
+        motorSpeed: 2,
+        minSpeed: 1,
+        maxSpeed: 5,
+        borderWidth: 0,
+        location: new Flora.Vector(this._world.width * 0.49, Math.random() * this._world.width),
+        color: [97,204,162],
+        opacity: 1,
+        width: 3,
+        height: 3
+      });
+
+      this._ceoDollars.push(ceoDollar);
     }
   }
-  _createCEO() {}
 }
 
